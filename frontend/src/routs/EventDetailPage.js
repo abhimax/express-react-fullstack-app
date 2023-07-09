c45fa5314cb316dd42e384b0009c1819fc73f9f0
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-07-10 01:36:58 +0530

    feat: add page component and route

---
+++ frontend/src/routs/EventDetailPage.js
@@ -0,0 +1,4 @@
+const EventDetailPage = () => {
+  return <h1>EventDetailPage</h1>;
+};
+export default EventDetailPage;
---
+++ frontend/src/routs/EventsPage.js
@@ -0,0 +1,4 @@
+const EventsPage = () => {
+  return <h1>EventsPage</h1>;
+};
+export default EventsPage;
