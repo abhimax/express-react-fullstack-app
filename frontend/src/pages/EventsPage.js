adfac1880a23f68c9d365fc44d6428227acd4fcb
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-07-16 18:08:38 +0530

    feat: error handling method 01

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -2,18 +2,22 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  {
+    return data.isError && <p>Error Occurs</p>;
+  }
+  const events = data.events;
   return <EventsList events={events} />;
 }
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/eventsx");
 
   if (!response.ok) {
-    //TODO
+    return { isError: true, message: "could not fetch events." };
   } else {
-    const resData = await response.json();
-    return resData.events;
+    //const resData = await response.json();
+    return response;
   }
 }
