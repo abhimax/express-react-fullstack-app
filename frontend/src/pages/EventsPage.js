96b5e3ef9e62ef0656df456b62db0f9258706b59
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-07-16 22:49:09 +0530

    feat: add config error page

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -3,23 +3,21 @@ import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
-  {
-    return data.isError && <p>Error Occurs</p>;
-  }
   const events = data.events;
   return <EventsList events={events} />;
 }
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/eventsx");
+  const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    //return { isError: true, message: "could not fetch events." };
-    //throw { message: "Could not fetch events!" };
-    throw new Response(JSON.stringify({ message: "Could not fetch events!" }), {
-      status: 500,
-    });
+    // METHOD 01 => return { isError: true, message: "could not fetch events." };
+    // METHOD 02 => throw { message: "Could not fetch events!" };
+    // METHOD 03 =>throw new Response(JSON.stringify({ message: "Could not fetch events!" }), {
+    //   status: 500,
+    // });
+    throw json({ message: "Could not fetch events!" }, { status: 500 });
   } else {
     //const resData = await response.json();
     return response;
