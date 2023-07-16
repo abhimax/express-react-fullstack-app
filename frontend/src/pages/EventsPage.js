0b5551d2aa75e707a2d9b247e38a5978d9cbb7b6
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-07-16 10:53:59 +0530

    feat: add event page to fetch data from backend

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,34 +1,37 @@
+import { useEffect, useState } from "react";
+
 import EventsList from "../components/EventsList";
 
-const eventList = [
-  {
-    id: "evt01",
-    image:
-      "https://w0.peakpx.com/wallpaper/235/27/HD-wallpaper-nature-landscape-landscape-nature.jpg",
-    title: "Event 01",
-    date: "September 01",
-  },
-  {
-    id: "evt02",
-    image:
-      "https://w0.peakpx.com/wallpaper/983/204/HD-wallpaper-nature-landscape.jpg",
-    title: "Event 02",
-    date: "03-03-2024",
-  },
-  {
-    id: "evt03",
-    image:
-      "https://w0.peakpx.com/wallpaper/615/949/HD-wallpaper-fall-autumn-beautiful-red-nature-beauty-landscapes-scenery.jpg",
-    title: "Event 03",
-    date: "03-03-2024",
-  },
-];
-const EventsPage = () => {
+function EventsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/events");
+
+      if (!response.ok) {
+        setError("Fetching events failed.");
+      } else {
+        const resData = await response.json();
+        setFetchedEvents(resData.events);
+      }
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
   return (
     <>
-      <h1>EventsPage</h1>
-      <EventsList events={eventList} />
+      <div style={{ textAlign: "center" }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
     </>
   );
-};
+}
+
 export default EventsPage;
