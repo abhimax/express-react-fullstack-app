17f455a3c34f5a5010893862b1221e359d522e66
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-07-16 13:35:49 +0530

    feat: clean loading state to handle in routs

--- frontend/src/pages/RootLayout.js
+++ frontend/src/pages/RootLayout.js
@@ -1,13 +1,11 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 const RootLayout = () => {
-  const navigation = useNavigate();
   return (
     <>
       <MainNavigation />
       <main>
-        {navigation.state === "loading" && <p>Loading...</p>}
         <Outlet />
       </main>
     </>
